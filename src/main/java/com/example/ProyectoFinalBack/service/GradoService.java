ab4d2aa3992da56939046dc9d73ecb30466a9828
abrah <0333231035@unjfsc.edu.pe>
2025-12-20 13:53:25 -0500

    implemente los servicios de GradoService

---
+++ src/main/java/com/example/ProyectoFinalBack/service/GradoService.java
@@ -0,0 +1,32 @@
+package com.example.ProyectoFinalBack.service;
+
+
+import com.example.ProyectoFinalBack.model.Grado;
+import com.example.ProyectoFinalBack.repository.GradoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class GradoService {
+
+    @Autowired
+    private GradoRepository repo;
+
+    public List<Grado> listar() {
+        return repo.findAll();
+    }
+
+    public Grado buscarPorId(Integer id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Grado registrar(Grado g) {
+        return repo.save(g);
+    }
+
+    // Método para eliminar (opcional, por si quieres gestionar grados desde el sistema)
+    public void eliminar(Integer id) {
+        repo.deleteById(id);
+    }
+}
