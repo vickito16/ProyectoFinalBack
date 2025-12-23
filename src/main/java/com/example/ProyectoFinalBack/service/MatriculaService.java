8628661b7252dd368086d2b0257ee04f758c2008
vickito16 <estefano.castellnos@gmail.com>
2025-12-23 14:18:40 -0500

    Se optimizo (eliminó y o midifico lienas de codigo inutilizados) MatriculaService

--- src/main/java/com/example/ProyectoFinalBack/service/MatriculaService.java
+++ src/main/java/com/example/ProyectoFinalBack/service/MatriculaService.java
@@ -23,22 +23,4 @@ public class MatriculaService {
     public boolean existeMatricula(Integer idAlumno, String anio) {
         return repo.existsByAlumno_IdAlumnoAndAnioAcademico(idAlumno, anio);
     }
-
-    // --- NUEVOS MÉTODOS DE BÚSQUEDA ---
-
-    public List<Matricula> buscarPorDni(String dni) {
-        return repo.findByAlumno_DniAlumno(dni);
-    }
-
-    public List<Matricula> listarPorAnio(String anio) {
-        return repo.findByAnioAcademico(anio);
-    }
-
-    public List<Matricula> listarPorGrado(Integer idGrado) {
-        return repo.findByGrado_IdGrado(idGrado);
-    }
-
-    public List<Matricula> filtrarPorAnioYGrado(String anio, Integer idGrado) {
-        return repo.findByAnioAcademicoAndGrado_IdGrado(anio, idGrado);
-    }
 }
